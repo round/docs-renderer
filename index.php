021b1309502533c66b3faec4bf7b76cf2b8af684
Maxim Leyzerovich <contact@round.is>
2020-05-14 02:55:36 -0700

    Pulling title and body in place

--- index.php
+++ index.php
@@ -1,13 +1,41 @@
 <?php
 
+$docID = $_GET['id'];
+$docURL = 'https://docs.google.com/document/d/e/' . $docID . '/pub';
+$docEmbed = $docURL . '?embedded=true';
+
+
 $d = new DOMDocument;
 $mock = new DOMDocument;
-$d->loadHTML(file_get_contents('https://docs.google.com/document/d/e/2PACX-1vQ2UTb-_kM3u9OW4ijIeQdUWSfoi40sLVTZ74TNfwD6B1Xrld6ZFZDKCww4VYI34nFtHe0z3Eyijn52/pub?embedded=true'));
-$body = $d->getElementsByTagName('body')->item(0);
+$d->loadHTML(file_get_contents($docURL));
+
+
+
+// $title = $d->getElementsByTagName('title')->item(0)->nodeValue; //can also be textContent
+// $body = $d->getElementsByTagName('body')->item(0);
+// $body = $d->getElementById('contents')->item(0);
+// foreach ($body->childNodes as $child){
+//     $mock->appendChild($mock->importNode($child, true));
+// }
+
+
+
+$title = $d->getElementsByTagName('title')->item(0)->nodeValue; //can also be textContent
+$body = $d->getElementById('contents');
 foreach ($body->childNodes as $child){
-    $mock->appendChild($mock->importNode($child, true));
+  $mock->appendChild($mock->importNode($child, true));
 }
 
-echo $mock->saveHTML();
+$renderedPage = $mock->saveHTML();
+
 
 ?>
+
+<html>
+  <head>
+    <title><?php echo $title ?></title>
+  </head>
+  <body>
+    <?php echo $renderedPage ?>
+  </body>
+</html>
